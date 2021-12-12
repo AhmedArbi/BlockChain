87f0c3c7fdb41fa606c767cfc403f7199ef2ee88
ahmed <ahmed>
2021-12-12 02:09:58 +0100

    set up metamask and ganache

---
+++ truffle/contracts/Election.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.0;
+contract Election {
+// Read/write candidate
+string public candidate;
+struct Candidate {
+uint id;
+string name;
+uint votes;
+string image;
+}
+// Read/write Candidates
+mapping(uint => Candidate) public candidates;
+// Store Candidates Count
+uint public candidatesCount;
+
+// Store accounts that have voted
+mapping(address => bool) public voters;
+
+event votedEvent (
+uint indexed _candidateId
+);
+
+// Constructor
+constructor() public {
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+    addCandidate("Candidate 3");
+
+}
+
+function addCandidate (string memory _name) private {
+candidatesCount ++;
+candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+}
+
+function AdminaddCandidate (string memory _name) public {
+addCandidate(_name);
+}
+
+function vote (uint _candidateId) public {
+// require that they haven't voted before
+require(!voters[msg.sender]);
+// require a valid candidate
+require(_candidateId > 0 && _candidateId <= candidatesCount);
+// record that voter has voted
+voters[msg.sender] = true;
+// update candidate vote Count
+candidates[_candidateId].voteCount ++;
+// trigger voted event
+emit votedEvent(_candidateId);
+}
+}
+
